c9c6b5e2f3f4974c194339f24dd4b1b1a6268fd8
Saul Gonzalez <gonzalezolisaul23@gmail.com>
2025-04-09 21:25:16 -0600

    Add files via upload

---
+++ source/references/patterns.go
@@ -0,0 +1,12 @@
+package references
+
+import "regexp"
+
+var PseudoElementsPtrn = regexp.MustCompile(`(?i)(code segment)|(data segment)|(stack segment)
+|(\.code segment)|(\.data segment)|(\.stack segment)|(\.model small)|(\.data)|(\.stack)|(\.code)|byte ptr|word ptr`)
+var BaseConstantPtrn = regexp.MustCompile(`\w$`)
+var StringPtrn = regexp.MustCompile(`(^")|(^')`)
+var ConstantPtrn = regexp.MustCompile(`(^\d)|(^")|(^')`)
+var FullStringPtrn = regexp.MustCompile(`("+(.)+")|('+(.)+')`)
+var DupPtrn = regexp.MustCompile(`(DUP)+\(+(.*)+\)`)
+var SymbolPtrn = regexp.MustCompile(`^\w\D`)
