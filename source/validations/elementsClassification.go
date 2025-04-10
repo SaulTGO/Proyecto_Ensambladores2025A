eda35c28c3bc1a02a3bd514b50c2826c6f8d6c6f
Saul Gonzalez <gonzalezolisaul23@gmail.com>
2025-04-09 21:21:35 -0600

    Actualizacion del archivo

--- source/validations/elementsClassification.go
+++ source/validations/elementsClassification.go
@@ -2,14 +2,13 @@ package validations
 
 import (
 	"Assembler/references"
-	"regexp"
 	"strings"
 )
 
 func CheckConstant(element string) (bool, string) {
 	base := ""
-	constantPtrn := regexp.MustCompile(`(^\d)|(^")|(^')`)
-	if constantPtrn.MatchString(element) {
+
+	if references.ConstantPtrn.MatchString(element) {
 		base = checkBaseConstant(element)
 		return true, base
 	}
@@ -18,10 +17,10 @@ func CheckConstant(element string) (bool, string) {
 }
 
 func checkBaseConstant(element string) string {
-	baseConstantPtrn := regexp.MustCompile(`\w$`)
+
 	base := ""
-	if baseConstantPtrn.MatchString(element) {
-		b := baseConstantPtrn.FindString(element)
+	if references.BaseConstantPtrn.MatchString(element) {
+		b := references.BaseConstantPtrn.FindString(element)
 
 		if strings.EqualFold(b, "b") {
 			base = "Binario"
@@ -29,11 +28,11 @@ func checkBaseConstant(element string) string {
 			base = "Hexadecimal"
 		} else if strings.EqualFold(b, "o") {
 			base = "Octal"
-		} else if strings.EqualFold(b, "") {
-
 		} else {
 			base = "Decimal"
 		}
+	} else if references.StringPtrn.MatchString(element) {
+		base = "Caracter"
 	}
 	return base
 }
@@ -58,18 +57,34 @@ func CheckRegister(element string) (bool, string) {
 
 func CheckPseudoInstruction(element string) (bool, string) {
 	_, found := references.PseudoInstructions[element]
-	if found {
+	isPseudo := references.PseudoElementsPtrn.MatchString(element)
+	if found || isPseudo {
+		return true, "PseudoInstruccion"
+	}
+	if references.DupPtrn.MatchString(element) {
 		return true, "PseudoInstruccion"
 	}
 	return false, ""
 }
 
 func CheckSegment(element string) (bool, string) {
-	segmentPtrns := regexp.MustCompile(`(?i)(code segment)|(?i)(data segment)|(?i)(stack segment)
-	|(?i)(\.code segment)|(?i)(\.data segment)|(?i)(\.stack segment)|(?i)(\.model small)`)
-
-	if segmentPtrns.MatchString(element) {
+	if references.PseudoElementsPtrn.MatchString(element) {
 		return true, "PseudoInstruccion"
+	}
+	return false, ""
+}
+
+func CheckInvalidInstruction(element string) (bool, string) {
+	_, found := references.InvalidInstructions[element]
+	if found {
+		return true, "No valido"
+	}
+	return false, ""
+}
+
+func CheckSymbol(element string) (bool, string) {
+	if references.SymbolPtrn.MatchString(element) {
+		return true, "Simbolo"
 	}
 	return false, ""
 }
