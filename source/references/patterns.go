ba941c8b1c3cb88d0675b8fc94fd6834170b20e7
Saul Gonzalez <gonzalezolisaul23@gmail.com>
2025-04-24 10:14:33 -0600

    Add files via upload

--- source/references/patterns.go
+++ source/references/patterns.go
@@ -2,11 +2,12 @@ package references
 
 import "regexp"
 
-var PseudoElementsPtrn = regexp.MustCompile(`(?i)(code segment)|(data segment)|(stack segment)
-|(\.code segment)|(\.data segment)|(\.stack segment)|(\.model small)|(\.data)|(\.stack)|(\.code)|byte ptr|word ptr`)
+var PseudoElementsPtrn = regexp.MustCompile(`(?i)(code segment)|(data segment)|(stack segment)|(\.code segment)|(\.data segment)|(\.stack segment)|(\.model small)|(\.data)|(\.stack)|(\.code)|byte ptr|word ptr`)
 var BaseConstantPtrn = regexp.MustCompile(`\w$`)
 var StringPtrn = regexp.MustCompile(`(^")|(^')`)
 var ConstantPtrn = regexp.MustCompile(`(^\d)|(^")|(^')`)
 var FullStringPtrn = regexp.MustCompile(`("+(.)+")|('+(.)+')`)
-var DupPtrn = regexp.MustCompile(`(DUP)+\(+(.*)+\)`)
+var DupPtrn = regexp.MustCompile(`(?i)(DUP)+\(+(.*)+\)`)
 var SymbolPtrn = regexp.MustCompile(`^\w\D`)
+var BadQuotesPtrn = regexp.MustCompile(`"[^"\n]*(?:$|[^"]+$)`)
+var EndQuotesPtrn = regexp.MustCompile(`"$|'$`)
